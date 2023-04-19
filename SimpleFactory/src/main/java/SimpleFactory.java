4da07a07f13dd48245e21e17dafa5d4ea924176a
KarlexYan <KarlexYan@aliyun.com>
2023-04-19 20:52:45 +0800

    简单工厂模式

---
+++ SimpleFactory/src/main/java/SimpleFactory.java
@@ -0,0 +1,55 @@
+/*
+ *  简单工厂模式
+ * */
+public class SimpleFactory {
+    public static void main(String[] args) {
+        Product productA = Factory.createProduct("A");
+        productA.info();
+
+        Product productB = Factory.createProduct("B");
+        productB.info();
+
+        Product productC = Factory.createProduct("C");
+        productC.info();
+    }
+}
+
+class Factory {
+    public static Product createProduct(String type) {
+        Product product = null;
+
+        switch (type) {
+            case "A":
+                product = new ProductA();
+                break;
+            case "B":
+                product = new ProductB();
+                break;
+            default:
+                System.out.println("没有" + type + "这个类型的产品");
+                break;
+        }
+
+        return product;
+    }
+}
+
+abstract class Product {
+    public abstract void info();
+}
+
+class ProductA extends Product {
+
+    @Override
+    public void info() {
+        System.out.println("产品的信息A");
+    }
+}
+
+class ProductB extends Product {
+
+    @Override
+    public void info() {
+        System.out.println("产品的信息B");
+    }
+}
