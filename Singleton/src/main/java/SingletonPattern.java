69edb62c91ac3514d61eb5f8035acb41b177f099
KarlexYan <KarlexYan@aliyun.com>
2023-04-22 14:01:55 +0800

    单例模式 补充

--- Singleton/src/main/java/SingletonPattern.java
+++ Singleton/src/main/java/SingletonPattern.java
@@ -5,13 +5,13 @@
 public class SingletonPattern {
     public static void main(String[] args) {
         Singleton singleton1 = Singleton.getInstance();
-        System.out.println(singleton1);
+        System.out.println(singleton1); //Singleton@3b07d329
 
         Singleton singleton2 = Singleton.getInstance();
-        System.out.println(singleton2);
+        System.out.println(singleton2); //Singleton@3b07d329
 
         Singleton singleton3 = Singleton.getInstance();
-        System.out.println(singleton3);
+        System.out.println(singleton3); //Singleton@3b07d329
     }
 }
 
