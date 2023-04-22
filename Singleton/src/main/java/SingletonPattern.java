82aa83f2d2d1192b22ea66ebf5dfef4a45448f00
KarlexYan <KarlexYan@aliyun.com>
2023-04-22 14:01:16 +0800

    单例模式

---
+++ Singleton/src/main/java/SingletonPattern.java
@@ -0,0 +1,26 @@
+/*
+*   单例模式
+*   意图：保证一个类仅有一个实例，并提供一个访问它的全局访问点
+* */
+public class SingletonPattern {
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        System.out.println(singleton1);
+
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton2);
+
+        Singleton singleton3 = Singleton.getInstance();
+        System.out.println(singleton3);
+    }
+}
+
+class Singleton{
+    private static Singleton instance = new Singleton();
+
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
