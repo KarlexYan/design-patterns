38ab711c80c74a13036ca8b6e3a4574d18adca6d
KarlexYan <KarlexYan@aliyun.com>
2023-04-26 17:33:31 +0800

    简单工厂模式 补充

--- SimpleFactory/src/main/java/SimpleFactory.java
+++ SimpleFactory/src/main/java/SimpleFactory.java
@@ -4,13 +4,13 @@
 public class SimpleFactory {
     public static void main(String[] args) {
         Product productA = Factory.createProduct("A");
-        productA.info();
+        productA.info(); // 产品的信息A
 
         Product productB = Factory.createProduct("B");
-        productB.info();
+        productB.info(); // 产品的信息B
 
         Product productC = Factory.createProduct("C");
-        productC.info();
+        productC.info(); // 没有C这个类型的产品
     }
 }
 
